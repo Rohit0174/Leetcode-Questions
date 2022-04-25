37401d4baf263a6e5bb02771baf6271443d19230
Rohit0174 <70084741+Rohit0174@users.noreply.github.com>
2022-04-25 12:43:36 +0530

    Time: 1 ms (68.02%), Space: 43.8 MB (6.04%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,39 +10,33 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode ans = new ListNode();
-        ListNode current = new ListNode();
-        if(list1==null)return list2;
-        if(list2==null)return list1;
-        if(list1.val<list2.val){
-            ans.val = list1.val;
-            list1=list1.next;
-        }else{
-            ans.val = list2.val;
-            list2=list2.next;
-        }
-        current=ans;
+        ListNode head=new ListNode(1);
+        ListNode current=head;
         while(list1!=null && list2!=null){
             if(list1.val<list2.val){
-                current.next = list1;
+                ListNode temp=new ListNode(list1.val);
                 list1=list1.next;
+                current.next=temp;
+                current=current.next;
             }else{
-                current.next = list2;
+                  ListNode temp=new ListNode(list2.val);
                 list2=list2.next;
+                current.next=temp;
+                current=current.next;
             }
-            current = current.next;
         }
         while(list1!=null){
-             current.next = list1;
+             ListNode temp=new ListNode(list1.val);
                 list1=list1.next;
-            current = current.next;
+                current.next=temp;
+                current=current.next;
         }
         while(list2!=null){
-             current.next = list2;
+             ListNode temp=new ListNode(list2.val);
                 list2=list2.next;
-                        current = current.next;
-
+                current.next=temp;
+                current=current.next;
         }
-        return ans;
+        return head.next;
     }
 }
